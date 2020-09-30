9ae485403149bbda2992ed2843cb2b42ff76e8ec
huang <317894842@qq.com>
2020-09-30 17:20:25 +0800

    9,11,118

--- src/main/java/com/example/leecode/Topic_11.java
+++ src/main/java/com/example/leecode/Topic_11.java
@@ -1,4 +1,21 @@
 package com.example.leecode;
 
 public class Topic_11 {
+
+    public static void main(String[] args) {
+        int[] height =new int[] {1,8,6,2,5,4,8,3,7};
+    }
+
+    public int maxArea(int[] height) {
+        int j = height.length-1;
+        int num1 = height[0];
+        int num2 = height[j];
+        for(int i=0;i<height.length;i++){
+            for(;j>i;j--){
+
+            }
+        }
+
+        return 0;
+    }
 }
--- src/main/java/com/example/leecode/Topic_9.java
+++ src/main/java/com/example/leecode/Topic_9.java
@@ -1,12 +1,27 @@
 package com.example.leecode;
 
+import java.util.Stack;
+
 public class Topic_9 {
 
     public static void main(String[] args) {
-
+        System.out.println(isPalindrome(1210221));
     }
 
-    public boolean isPalindrome(int x) {
-        return false;
+    public static boolean isPalindrome(int x) {
+        Integer init = x;
+        if(x<0){
+            return false;
+        }
+        if(x/10==0){
+            return true;
+        }
+        int result = x%10;
+        do{
+            x/=10;
+            result *= 10;
+            result += (x%10);
+        }while (x>=10);
+        return result==init;
     }
 }
